4febacc1b4b4e7cbc58d48cb32d64f771463b1f7
fredjhosnick <36901848+fredjhosnick@users.noreply.github.com>
2023-06-09 10:54:46 +0200

    first commit

---
+++ src/pages/Policy.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Policy = () => {
+  return (
+    <Layout tilte={"Privacy Pilicy"}>
+    <h1>Privacy Policy Page</h1>
+</Layout>
+  )
+}
+
+export default Policy
